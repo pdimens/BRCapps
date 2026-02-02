8c6a3427f03fe968bf7f449b5eda9e2a4b495575
pdimens <pdimens@gmail.com>
2026-02-02 10:56:07 -0500

    fix invalid table

--- apps/DNA_dilutions.py
+++ apps/DNA_dilutions.py
@@ -69,15 +69,19 @@ def _(mo):
 @app.cell
 def _(copy, df, pd, start_ul, target_ng):
     # define functions
+    def print_upto(x, n) -> list[str]:
+        '''Given a stringio bytes object `x`, split out lines and return up to `n` lines'''
+        _lines = x.decode("utf-8").split('\n')
+        return _lines[:min(n, len(_lines))]
 
-    # Function to parse well notation (handles both A1 and A01 as column 1)
     def parse_well(well):
+        '''Function to parse well notation (handles both A1 and A01 as column 1)'''
         row = well[0]  # First character is the row (A-H)
         col = str(int(well[1:]))  # Remaining characters are the column number
         return row, col
 
-    # Only style cells in the H2O column where value < 0
     def style_cell(rowId, columnName, value):
+        '''Only style cells in the diluent column where value < 0'''
         if columnName == 'diluent (ul)':
             if value < 0:
                 return {
@@ -94,6 +98,7 @@ def _(copy, df, pd, start_ul, target_ng):
         return {}
 
     def style_well(rowId, columnName, value):
+        '''Only style cells where value is <0 or >190'''
         if columnName != 'row':
             if value < 0:
                 return {
@@ -101,7 +106,7 @@ def _(copy, df, pd, start_ul, target_ng):
                     "color": "darkred",
                     "fontWeight": "bold"
                 }
-            elif  value > (200 - start_ul.value):
+            elif  value > (190 - start_ul.value):
                 return {
                     "backgroundColor": "orange",
                     "color": "brown",
@@ -110,22 +115,23 @@ def _(copy, df, pd, start_ul, target_ng):
         return {}
 
     def recalc_dilution(updated_df):
-        # updated_df is the full edited table
+        '''callback function for editable dataframe'''
         updated_df['diluent (ul)'] = round(((updated_df['ng/ul'] * updated_df['ul DNA']) / target_ng.value) - updated_df['ul DNA'], 2)
 
     def table_to_plate(input_table) -> pd.DataFrame:
+        '''convert long-form table into 96-well plate format dataframe'''
         df_copy = copy.copy(input_table)
         # Extract row and column from well notation
         df_copy['row'] = df['well'].apply(lambda x: parse_well(x)[0])
         df_copy['col'] = df['well'].apply(lambda x: parse_well(x)[1])
-    
+
         # Create the transposed table
         plate_layout = df_copy.pivot(index='row', columns='col', values='diluent (ul)')
-    
+
         # Ensure all rows A-H and columns 1-12 exist (fill missing with 0)
         all_rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
         all_cols = [str(i) for i in range(1, 13)]
-    
+
         return plate_layout.reindex(index=all_rows, columns=all_cols).fillna(0)
     return recalc_dilution, style_cell, style_well, table_to_plate
 
@@ -165,20 +171,37 @@ def _(file_import, io, mo, pd):
         mo.md("/// admonition| Input file required\n///")
     )
 
-    df = pd.read_table(io.BytesIO(file_import.value[0].contents), engine='python', sep=None)
-    df.columns = [i.lower() for i in df.columns]
-    mo.stop(
-        any([i not in df.columns for i in ['well', 'sample', 'ng/ul']]),
-        output= mo.md(f"""
+    try:
+        df = pd.read_table(io.BytesIO(file_import.value[0].contents), engine='python', sep=None)
+        df.columns = [i.lower() for i in df.columns]
+    except Exception:
+        is_err = True
+        err_md = mo.md(f"""
         /// error| Invalid input file
 
-        The input file is expected to have a specific format as outlined above (columns `well`, `sample`, and `ng/ul`),
-        but those columns were not found. Instead, the input file has columns:
+        The `pandas` parser failed to read the input file. Please check that it conforms to a tabular comma or whitespace (tab/space) delimited file. The first 200 bytes of the uploaded file:
 
-        {list(df.columns)}
         ///
         """)
+        err = mo.vstack([err_md, mo.md(f"```\n{file_import.value[0].contents[:200]}\n```")])
+        mo.stop(is_err, err)
+
+    mo.stop(
+        any([i not in df.columns for i in ['well', 'sample', 'ng/ul']]),
+        mo.vstack([
+            mo.md(f"""
+                /// error| Invalid input file
+        
+                The input file is expected to have a specific format as outlined above (columns `well`, `sample`, and `ng/ul`),
+                but those columns were not found. 
+        
+                ///
+                """),
+            mo.ui.table(df.head(2), show_column_summaries=False, selection=None, show_data_types=False)
+        ])
     )
+
+
     return (df,)
 
 
@@ -191,24 +214,19 @@ def _(df, start_ul, target_ng):
 
 
 @app.cell
-def _(mo):
-    mo.md(r"""
- 
-    """)
-    return
-
-
-@app.cell
 def _(df_clean, file_import, mo, recalc_dilution, start_ul):
     mo.stop(not file_import.value)
-    mo.md(r"""
+    input_text = mo.md(r"""
     ## Input Volume
-    Use the slider to set a fixed input volume or the table editor to modify the input DNA volume  per sample. The changes will be reflected in the final output tables."
+    Use the slider to set a fixed input volume or the table editor to modify the input DNA volume  per sample. The changes will be reflected in the final output tables.
     """)
 
 
     editor = mo.ui.data_editor(df_clean, editable_columns=['ul DNA'], on_change=recalc_dilution)
-    mo.ui.tabs({"Fixed volume": start_ul, "Variable volume": editor})
+    mo.vstack([
+        input_text,
+        mo.ui.tabs({"Fixed volume": start_ul, "Variable volume": editor})
+    ])
     return (editor,)
 
 
